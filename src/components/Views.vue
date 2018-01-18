d21c2fa85ddda8b1118174a5b435e3c776f336e0
huangzhengjie <huangzhengjie@dingtalk.com>
2018-01-18 22:39:20 +0800

    添加全局路由钩子 登录验证拦截器

--- src/components/Views.vue
+++ src/components/Views.vue
@@ -15,6 +15,17 @@ export default {
 	name:'info',
 	data(){
 		return {}
+	},beforeRouteEnter (to, from, next) {
+	    // 在渲染该组件的对应路由被 confirm 前调用
+	    // 不！能！获取组件实例 `this`
+	    // 因为当守卫执行前，组件实例还没被创建
+	    // console.log(to, from, next)
+	    //  此时没有 $router
+	   
+	    setTimeout(function(){
+	    	$router.go(-1);
+	    },2000);
+	    next();
 	}
 }
 </script>
